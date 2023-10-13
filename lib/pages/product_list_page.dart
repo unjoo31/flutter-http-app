85f94f14ce993ebe18f04e055089f05d1af587a9
unjoo31 <unjoo31@gmail.com>
2023-10-13 17:23:00 +0900

    git commit "product 개별 통신"

--- lib/pages/product_list_page.dart
+++ lib/pages/product_list_page.dart
@@ -8,27 +8,24 @@ class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<Product>>(
-        future: ProductRepository().fetchProductList(),
+      body: FutureBuilder<Product>(
+        future: ProductRepository().fetchProduct(1),
         builder: (context, snapshot) {
-          print("그림그려짐");
-          if (snapshot.hasData) {
-            // 통신이 끝남
-            List<Product> productList =
-                snapshot.data ?? []; // 통신이 끝났어도 데이터가 없으면 null일 수 있다
+          print("그림 그려짐");
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            Product product = snapshot.data!;
             return ListView.builder(
-              itemCount: productList.length,
+              itemCount: 1, // 단일 항목이므로 itemCount는 1로 설정
               itemBuilder: (context, index) {
                 return ListTile(
-                  leading: Text("${productList[index].id}"),
-                  title: Text("${productList[index].name}"),
-                  subtitle: Text("${productList[index].price}"),
+                  leading: Text("${product.id}"),
+                  title: Text("${product.name}"),
+                  subtitle: Text("${product.price}"),
                 );
               },
             );
-          } else {
-            // 통신중인 상태
-            return Center(child: CircularProgressIndicator());
           }
         },
       ),
